4dc21726b5ca43fcec389e84b335719e0be21ddc
AwesomeLi <AwesomeLiwork@163.com>
2020-11-04 11:11:36 +0800

    登陆页面初始化完成

---
+++ src/components/Login.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="login_contanier">
+    <div class="login_box">
+      <h1>Login</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="less" scoped>
+.login_contanier {
+  height: 100%;
+  background-color: #2b4b6b;
+}
+.login_box {
+  width: 450px;
+  height: 350px;
+  background-color: #fff;
+  border-radius: 3px;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+}
+</style>
