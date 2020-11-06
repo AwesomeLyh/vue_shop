c461439007c77b61d3b6d7bb493759e1593649c6
AwesomeLi <AwesomeLiwork@163.com>
2020-11-06 10:26:47 +0800

    挂载请求拦截器，添加请求头token

--- src/main.js
+++ src/main.js
@@ -15,6 +15,12 @@ import axios from "axios";
 
 axios.defaults.baseURL = "http://localhost:8888/api/private/v1/";
 
+axios.interceptors.request.use(config => {
+  console.log(config);
+  config.headers.Authorization = window.sessionStorage.getItem("token");
+  return config;
+});
+
 //给Vue的原型对象添加一个属性
 Vue.prototype.$http = axios;
 
