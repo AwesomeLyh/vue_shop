66c93aed13c05cbce0e9f537ee7768a7dc26b689
AwesomeLi <AwesomeLiwork@163.com>
2020-11-15 15:21:50 +0800

    使用@babel/plugin-syntax-dynamic-import实现路由懒加载

--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,33 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Login from '../components/Login.vue'
-import Home from '../components/Home.vue'
-import Welcome from '../components/Welcome.vue'
-import Users from '../components/user/Users.vue'
-import Roles from '../components/power/Roles.vue'
-import CateGories from '../components/goods/CateGories.vue'
-import Params from '../components/goods/Params.vue'
-import Goods from '../components/goods/GoodsList.vue'
-import AddGood from '../components/goods/Add.vue'
-import Order from '../components/order/Order.vue'
-import Report from '../components/report/Report'
-Vue.use(Router)
+
+// import Login from './components/Login.vue'
+const Login = () => import(/* webpackChunkName: "login_home_welcome" */ '../components/Login.vue')
+// import Home from './components/Home.vue'
+const Home = () => import(/* webpackChunkName: "login_home_welcome" */ '../components/Home.vue')
+// import Welcome from './components/Welcome.vue'
+const Welcome = () => import(/* webpackChunkName: "login_home_welcome" */ '../components/Welcome.vue')
+
+// import Users from './components/user/Users.vue'
+const Users = () => import(/* webpackChunkName: "Users_Rights_Roles" */ '../components/user/Users.vue')
+// import Rights from './components/power/Rights.vue'
+const Roles = () => import(/* webpackChunkName: "Users_Rights_Roles" */ '../components/power/Roles.vue')
+
+// import Cate from './components/goods/Cate.vue'
+const CateGories = () => import(/* webpackChunkName: "Cate_Params" */ '../components/goods/CateGories.vue')
+// import Params from './components/goods/Params.vue'
+const Params = () => import(/* webpackChunkName: "Cate_Params" */ '../components/goods/Params.vue')
+
+// import GoodsList from './components/goods/List.vue'
+const Goods = () => import(/* webpackChunkName: "GoodsList_Add" */ '../components/goods/GoodsList.vue')
+// import Add from './components/goods/Add.vue'
+const AddGood = () => import(/* webpackChunkName: "GoodsList_Add" */ '../components/goods/Add.vue')
+
+// import Order from './components/order/Order.vue'
+const Order = () => import(/* webpackChunkName: "Order_Report" */ '../components/order/Order.vue')
+// import Report from './components/report/Report.vue'
+const Report = () => import(/* webpackChunkName: "Order_Report" */ '../components/report/Report.vue')
+
 const router = new Router({
   routes: [
     {
@@ -37,6 +53,8 @@ const router = new Router({
     }
   ]
 })
+Vue.use(Router)
+
 // 挂载路由守卫
 router.beforeEach((to, from, next) => {
   if (to.path === '/login') return next()
